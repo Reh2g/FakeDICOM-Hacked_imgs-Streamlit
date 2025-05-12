080e8e1bcfdb4f75951debc165577b8541e70c0b
Gabriel Missima <gnmissima@gmail.com>
2025-05-12 02:16:18 -0300

    Att. 12-05-2025

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,142 +1,224 @@
-from PIL import Image, ImageOps
-from keras.models import load_model
-
-import matplotlib
-import numpy as np
 import streamlit as st
-import tensorflow as tf
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 import cv2
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+import io
+import time
 
-def load_image(img):
-    im = Image.open(img)
-    im = im.resize([224,224])
-    return im
+# ----------------- FUNÇÕES -----------------
+def freq_spec(fshift, image, threshold=5, add_noise=True, corner=0):
+    fshift = fshift.copy()
+    if add_noise:
+        rows, cols = image.shape
+        noise_size = int(np.sqrt(threshold * rows * cols))
+        sigma = 2
 
-st.title("Fake-DICOM: Uso de redes neurais profundas para avaliar se a combinação de algoritmos de criptografia com transformadas temporais aumenta a segurança de imagens DICOM contra tentativas intrusão maliciosas")
-st.header("Envie um exame médico qualquer .jpg ou .png", divider="gray")
+        def gaussian_blur(size, sigma):
+            ax = np.linspace(-(size - 1) / 2., (size - 1) / 2., size)
+            xx, yy = np.meshgrid(ax, ax)
+            kernel = np.exp(-(xx**2 + yy**2) / (2. * sigma**2))
+            return kernel / np.sum(kernel)
 
-Model_CNN = tf.keras.models.load_model('model_fold_1.keras')
+        blur_kernel = gaussian_blur(noise_size, sigma)
 
-for layer in Model_CNN.keras.layers:
-    layer.trainable = False
+        def blur_patch(spectrum, r_start, c_start):
+            patch = spectrum[r_start:r_start+noise_size, c_start:c_start+noise_size]
+            if patch.shape != blur_kernel.shape:
+                return
+            real_blurred = cv2.filter2D(np.real(patch), -1, blur_kernel)
+            imag_blurred = cv2.filter2D(np.imag(patch), -1, blur_kernel)
+            spectrum[r_start:r_start+noise_size, c_start:c_start+noise_size] = real_blurred + 1j * imag_blurred
 
-uploadFile = st.file_uploader(label="Upload image", type=['jpg', 'png'])
-
-def generate_heatmap(model, sample_image):
-    sample_image_exp = np.expand_dims(sample_image, axis=0)
-    
-    intermediate_model = tf.keras.models.Model(inputs=model.input, outputs=model.get_layer('last_conv').output)
-    activations = intermediate_model.predict(sample_image_exp)
-    activations = tf.convert_to_tensor(activations)
-
-    predictions = model.predict(sample_image_exp)
-
-    with tf.GradientTape() as tape:
-        iterate = tf.keras.models.Model([model.input], [model.output, model.get_layer('last_conv').output])
-        model_out, last_conv_layer = iterate(sample_image_exp)
-        class_out = model_out[:, np.argmax(model_out[0])]
-        tape.watch(last_conv_layer)
-        grads = tape.gradient(class_out, last_conv_layer)
-
-    if grads is None:
-        raise ValueError('Gradients could not be computed. Check the model and layer names.')
-
-    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
-    pooled_grads = tf.where(pooled_grads == 0, tf.ones_like(pooled_grads) * 1e-10, pooled_grads)
-    
-    heatmap = tf.reduce_mean(tf.multiply(pooled_grads, last_conv_layer[0]), axis=-1)
-
-    min_value = np.min(heatmap)
-    max_value = np.max(heatmap)
-
-    heatmap = (heatmap - min_value) / (max_value - min_value)
-    heatmap = np.asarray(heatmap)
-    heatmap = (heatmap - 1) * (-1)
-
-    heatmap_resized = cv2.resize(heatmap, (sample_image.shape[1], sample_image.shape[0]))
-    heatmap_resized = np.uint8(255 * heatmap_resized)
-    
-    heatmap_colored = matplotlib.cm.jet(heatmap_resized)[:, :, :3]
-    heatmap_colored = np.uint8(heatmap_colored * 255)
-    
-    alpha_channel = np.uint8(heatmap_resized)
-    heatmap_colored_with_alpha = np.dstack((heatmap_colored, alpha_channel))
-    
-    sample_image_uint8 = np.uint8(255 * np.squeeze(sample_image))
-    image_rgb = cv2.cvtColor(sample_image_uint8, cv2.COLOR_GRAY2RGB)
-    image_rgba = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2RGBA)
-    
-    alpha_factor = alpha_channel / 255.0
-    for c in range(0, 3):
-        image_rgba[..., c] = image_rgba[..., c] * (1 - alpha_factor) + heatmap_colored[..., c] * alpha_factor
-    
-    return image_rgba
-
-def CNN_predict(input_img, Model_CNN):
-    dense_CNN = Model_CNN.get_layer('dense').output
-    dense_CNN_extractor = tf.keras.models.Model(inputs=Model_CNN.input, outputs=dense_CNN)
-
-    dense_features = dense_CNN_extractor.predict(input_img)
-    dense_features = dense_features.reshape(dense_features.shape[0], -1)
-
-    pred_confidence_CNN = Model_CNN.predict(dense_features)
-    pred_class_labels_CNN = np.argmax(pred_confidence_CNN, axis=1)
-
-    return pred_confidence_CNN, pred_class_labels_CNN
-
-if uploadFile is not None:
-    img = load_image(uploadFile)
-    st.image(img)
-    hide_img_fs = '''
-    <style>
-    button[title="View fullscreen"]{
-        visibility: hidden;}
-    </style>
-    '''
-
-    if st.button('Diagnosis'):
-        X = Image.open(uploadFile)
-        X = ImageOps.grayscale(X)
-
-        img_array = np.array(X)
-        
-        st.markdown(hide_img_fs, unsafe_allow_html=True)
-        st.write("Image Uploaded Successfully")
-        
-        input_shape = Model_CNN.input_shape[1:-1]
-        h, w = input_shape
-        
-        image_resized = cv2.resize(img_array, (w, h))
-        image_normalized = image_resized.astype('float32') / 255.0
-        
-        image_normalized = np.expand_dims(image_normalized, axis=-1)
-        sample_image_exp = np.expand_dims(image_normalized, axis=0)
-
-# Predição CNN
-        
-        prediction, y_pred = CNN_predict(sample_image_exp, Model_CNN)
-
-        print(prediction.max())
-        print(y_pred[0])
-        
-        if(y_pred[0] == 0):
-            st.subheader("Normal")
-            st.write("Esta imagem tem " + str("{:.2f}".format(prediction[0].max()*100)+"% de ser segura para ser aberta."))
+        if corner == 0:
+            blur_patch(fshift, 0, 0)
+        elif corner == 1:
+            blur_patch(fshift, 0, cols - noise_size)
+        elif corner == 2:
+            blur_patch(fshift, rows - noise_size, 0)
         else:
-            st.subheader("Hackeada")
-            st.write("Esta imagem tem " + str("{:.2f}".format(prediction[0].max()*100)+"% de conter informações maliciosas."))
+            blur_patch(fshift, rows - noise_size, cols - noise_size)
 
-# Predições Conv4
-        
-        pred_CNN = Model_CNN.predict(sample_image_exp)
-        
-        pred_CNN_class = np.argmax(pred_CNN[0])
-        CNN_confidence = pred_CNN[0][pred_CNN_class]
+    magnitude_spectrum = 20 * np.log(np.abs(fshift) + 1)
+    magnitude_spectrum_norm = cv2.normalize(magnitude_spectrum, None, 0, 255, cv2.NORM_MINMAX)
+    return magnitude_spectrum_norm.astype(np.uint8)
 
-# Heatmap
 
-        heatmap_image = generate_heatmap(Model_CNN, image_normalized)
+def criptografar_imagem(fshift, aes_key):
+    inicio = time.perf_counter()
+    buffer = io.BytesIO()
+    np.save(buffer, fshift)
+    buffer.seek(0)
+    cipher_aes = AES.new(aes_key, AES.MODE_EAX)
+    ciphertext, tag = cipher_aes.encrypt_and_digest(buffer.read())
 
-        st.image(heatmap_image)
-else:
-    st.write("Por gentileza, envie um arquvo em .jpg ou .png.")
+    out_buffer = io.BytesIO()
+    out_buffer.write(cipher_aes.nonce)
+    out_buffer.write(tag)
+    out_buffer.write(ciphertext)
+    out_buffer.seek(0)
+    fim = time.perf_counter()
+    return out_buffer, fim - inicio
+
+
+def descriptografar_imagem(chave, arquivo):
+    inicio = time.perf_counter()
+    chave.seek(0)
+    arquivo.seek(0)
+    aes_key = chave.read()
+    enc_bytes = arquivo.read()
+
+    nonce = enc_bytes[:16]
+    tag = enc_bytes[16:32]
+    ciphertext = enc_bytes[32:]
+    cipher_aes = AES.new(aes_key, AES.MODE_EAX, nonce)
+    decrypted_data = cipher_aes.decrypt_and_verify(ciphertext, tag)
+    buffer = io.BytesIO(decrypted_data)
+    fshift_restaurado = np.load(buffer)
+    fim = time.perf_counter()
+    return fshift_restaurado, fim - inicio
+
+
+def ifft(fshift):
+    imagem_restaurada = np.abs(np.fft.ifft2(np.fft.ifftshift(fshift)))
+    imagem_restaurada_uint8 = cv2.normalize(imagem_restaurada, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+    return imagem_restaurada_uint8
+# ----------------- INTERFACE -----------------
+# Carrega a imagem de um arquivo local
+image = Image.open("banner.jpg")
+st.image(image, use_container_width =True)
+
+st.title("Fake-DICOM: Detecção de Anomalias em Imagens Médicas")
+
+# ----- ESTATÍSTICAS -----
+st.subheader("Estatísticas da Criptografia")
+st.caption("Último teste realizado com o algoritmo AES para geração de estatísticas: 10/05/2025")
+df = pd.read_excel(r'C:\Users\CarlosChinen\OneDrive\Main\FEI\TCC\Streamlit\cripto.xlsx')
+st.dataframe(df.describe())
+
+fig, ax = plt.subplots(figsize=(12, 5))
+ax.plot(df['slice'], df['tempoExecucaoAES'], color='blue')
+ax.set_title('Execução da Criptografia por Slice - 1799 Slices')
+ax.set_xticks([])
+ax.tick_params(axis='y', labelsize=10)
+ax.set_xlabel('Slice', fontsize=10)
+ax.set_ylabel('Tempo de Execução (s)', fontsize=10)
+ax.legend(['Tempo de Execução'])
+st.pyplot(fig)
+
+st.markdown("---")
+
+# ----- UPLOAD DE IMAGEM -----
+st.title("Upload de Imagem PNG")
+arquivo_imagem = st.file_uploader("Escolha um arquivo PNG para ser analisado", type=["png"])
+
+if arquivo_imagem:
+    imagem_upload = Image.open(arquivo_imagem)
+    imagem_np = np.array(imagem_upload)
+    imagem = cv2.resize(imagem_np, (512, 512), interpolation=cv2.INTER_AREA)
+    f = np.fft.fft2(imagem)
+    fshift = np.fft.fftshift(f)
+
+    mag_spec = 20 * np.log(np.abs(fshift) + 1)
+    mag_spec_norm = cv2.normalize(mag_spec, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+
+    st.success("Imagem carregada com sucesso e Transformada para o domínio da frequência!")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image(imagem, caption="Imagem carregada", use_container_width=True)
+    with col2:
+        st.image(mag_spec_norm, caption="Espectro Gerado", use_container_width=True)
+
+    st.markdown("---")
+    # ----- CRIPTOGRAFIA -----
+    st.header("🔐 Criptografia AES!")
+    if 'aes_key' not in st.session_state:
+        st.session_state.aes_key = get_random_bytes(32)
+
+    st.markdown('<h4>🔑 Sua chave de Criptografia AES (salve):</h4>', unsafe_allow_html=True)
+    st.code(st.session_state.aes_key.hex(), language="text")
+
+    st.download_button("🔑 Baixar Chave de Criptografia", st.session_state.aes_key, "aes_key.pem", "application/octet-stream")
+    
+    st.markdown("---")
+    st.markdown('### ▶️ Executar!')
+    if st.button("🔒 Executar Criptografia"):
+        cripto, tempo = criptografar_imagem(fshift, st.session_state.aes_key)
+        st.session_state.cripto = cripto
+        st.session_state.tempo_cripto = tempo
+
+    if 'cripto' in st.session_state:
+        st.markdown('<h4>🔒 30 primeiros bytes do arquivo criptografado (salve):</h4>', unsafe_allow_html=True)
+        st.code(st.session_state.cripto.getvalue()[:30], language="text")
+
+        st.download_button("🗃️ Baixar Arquivo Criptografado (.enc)", st.session_state.cripto, "imagem_criptografada.enc", "application/octet-stream")
+        st.success(f"✅ Criptografia concluída em {st.session_state.tempo_cripto:.4f} segundos.")
+
+    st.markdown("---")
+    # ----- DESCRIPTOGRAFIA -----
+    st.header("🔓 Descriptografia AES!")
+    st.markdown('<h4>🔑 Insira a Chave para Descriptografia</h4>', unsafe_allow_html=True)
+    chave_descript = st.file_uploader("Chave", type=["pem"])
+
+    st.markdown('<h4>🗃️ Insira o Arquivo Criptografado</h4>', unsafe_allow_html=True)
+    enc_file = st.file_uploader("Arquivo Criptografado", type=["enc"])
+
+
+    if chave_descript and enc_file:
+        try:
+            fshift_restaurado, tempo_decript = descriptografar_imagem(chave_descript, enc_file)
+            st.success(f"✅ Descriptografia concluída em {tempo_decript:.4f} segundos.")
+
+
+            col_central = st.columns([1, 2, 1])[1]  # Cria três colunas e usa a do meio
+            with col_central:
+                st.subheader('Imagem Restaurada!')
+                st.image(ifft(fshift_restaurado), width=300)
+        except Exception as e:
+            st.error(f"❌ Erro na descriptografia: {e}")
+
+    st.markdown("---")
+    # ----- INICIAR CNN -----
+    if 'cnn_ativa' not in st.session_state:
+        st.session_state.cnn_ativa = False
+
+    if st.button("Iniciar CNN"):
+        st.session_state.cnn_ativa = True
+
+    if st.session_state.cnn_ativa:
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image(imagem, caption="Imagem carregada", use_container_width=True)
+        with col2:
+            st.image(mag_spec_norm, caption="Espectro Gerado", use_container_width=True)
+
+        st.subheader("Escolha uma opção par posicionar a anomalia:")
+        col1, col2, col3, col4 = st.columns(4)
+
+        if col1.button("Superior Esquerdo"): # DAR UM JEITO DE CHAMAR A CNN PRA CADA UMA DESSAS OPÇÕES (JÁ TEM O FSHIFT E A IMAGEM)
+            col_central = st.columns([1, 2, 1])[1]
+            with col_central:
+                st.image(freq_spec(fshift, imagem, threshold=5/100, add_noise=True, corner=0), caption="Imagem Alterada", width=300)
+
+        if col2.button("Inferior Esquerdo"):
+            col_central = st.columns([1, 2, 1])[1]
+            with col_central:
+                st.image(freq_spec(fshift, imagem, threshold=5/100, add_noise=True, corner=2), caption="Imagem Alterada", width=300)
+
+        if col3.button("Superior Direito"):
+            col_central = st.columns([1, 2, 1])[1]
+            with col_central:
+                st.image(freq_spec(fshift, imagem, threshold=5/100, add_noise=True, corner=1), caption="Imagem Alterada", width=300)
+
+        if col4.button("Inferior Direito"):
+            col_central = st.columns([1, 2, 1])[1]
+            with col_central:
+                st.image(freq_spec(fshift, imagem, threshold=5/100, add_noise=True, corner=3), caption="Imagem Alterada", width=300)
+    
+st.markdown("""<hr style="border:1px solid gray">""", unsafe_allow_html=True)
+st.caption("TCC - Ciência da Computação - FEI")
+st.caption("Projeto desenvolvido por Carlos M. H. Chinen, Gabriel N. Missima, Vinicius A. Pedro")
+st.caption("Orientador: Prof. Dr. Paulo Sérgio")
