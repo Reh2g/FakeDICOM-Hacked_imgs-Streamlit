088adfcc4371a5264a0cfb6930a3be2c8b43d583
Gabriel Missima <gnmissima@gmail.com>
2025-05-12 18:47:11 -0300

    Att. 12-05-2025

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,13 @@
-import streamlit as st
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 from PIL import Image
+
+import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 import cv2
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
 import io
 import time
 
