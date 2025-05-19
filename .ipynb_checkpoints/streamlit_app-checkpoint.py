a60865896fbbfc97c1f9abe42a165e873fffb153
Gabriel Missima <gnmissima@gmail.com>
2025-05-18 23:21:35 -0300

    Att. 18-05-2025
    Funciona, pelo amor de Deus

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -41,14 +41,14 @@ def preprocessar_imagem(imagem):
     img = img.astype('float32') / 255.0
     return img
 
-def freq_spec(image, threshold=0.05, add_noise=True, corner=0):
+def freq_spec(image, threshold, add_noise=True, corner):
     f = np.fft.fft2(image)
     fshift = np.fft.fftshift(f)
 
     if add_noise:
         rows, cols = image.shape
         noise_size = int(np.sqrt(threshold * rows * cols))
-        sigma = 0.8  # Igual ao código de treinamento
+        sigma = 0.8
 
         def gaussian_blur(size, sigma):
             ax = np.linspace(-(size - 1) / 2., (size - 1) / 2., size)
