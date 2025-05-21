b4e9574e96f5a221d746ba070d76b9085518875f
Carlos Massato <massatohc@gmail.com>
2025-05-21 13:59:12 -0300

    FINAL FANTASY IIIIII

--- streamlit_app.py
+++ streamlit_app.py
@@ -163,7 +163,6 @@ def ifft(fshift):
     return imagem_restaurada_uint8
 
 # ----------------- INTERFACE -----------------
-st.caption('TCC')
 image = Image.open("banner.jpg")
 st.image(image, use_container_width =True)
 
